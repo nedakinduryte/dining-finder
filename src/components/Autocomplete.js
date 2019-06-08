0de8b7169593460f5f7827f973778f3d628b7081
nedakinduryte <neda.kinduryte@gmail.com>
2019-06-08 16:37:26 +0100

    added autosuggest component

---
+++ src/components/Autocomplete.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import Autosuggest from 'react-autosuggest';
+import _ from 'lodash';
+
+
+const API_KEY = "2d0e89daf27dd516eb7dcf5208bd83de";
+
+class Autocomplete extends React.Component {
+	constructor() {
+		super();
+
+		this.state = {
+			value: "",
+			suggestions: []
+		};
+
+		this.debouncedLoadSuggestions = _.debounce(this.loadSuggestions, 1000); // 1000ms is chosen for demo purposes only.
+	};
+
+	loadSuggestions = async value => {
+	  	const inputValue = value.trim().toLowerCase();
+	  	const inputLength = inputValue.length;
+
+	  	if (inputLength < 2) {
+	  		return [];
+	  	} else {
+			const api_call = await fetch(`https://developers.zomato.com/api/v2.1/cities?q=${value}`, 
+										 { headers: { 'Content-Type': 'application/json', "user-key": API_KEY }});
+			const data = await api_call.json();
+			this.setState({ suggestions: data.location_suggestions });
+		};
+	};
+
+	getSuggestionValue = suggestion => suggestion.name;
+
+	renderSuggestion = suggestion => {
+		return <div>{ suggestion.name }</div>
+	};
+
+	onChange = (event, { newValue }) => {
+		this.setState({
+		   	value: newValue
+		});
+	};
+
+	// Autosuggest will call this function every time you need to update suggestions.
+	onSuggestionsFetchRequested = ({ value }) => {
+	    this.debouncedLoadSuggestions(value);
+	};
+
+	// Autosuggest will call this function every time you need to clear suggestions.
+	onSuggestionsClearRequested = () => {
+		this.setState({
+			suggestions: []
+		});
+	};
+
+	render() {
+		return (
+			<Autosuggest
+		        suggestions={this.state.suggestions}
+		        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+		        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+		        getSuggestionValue={this.getSuggestionValue}
+		        renderSuggestion={this.renderSuggestion}
+		        inputProps={{
+				    placeholder: 'Type a city',
+				    value: this.state.value,
+				    onChange: this.onChange
+				}}
+		    />
+		)
+	}
+}
+
+
+export default Autocomplete;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,29 +1,38 @@
 import React from 'react';
-import queryString from 'query-string';
+//import queryString from 'query-string';
+import Autocomplete from './Autocomplete';
 
+
+
+
+// component autocomplete
+// render only autosuggest
+// pass in api_call as prop
 
 class Form extends React.Component {
 
-	handleSubmit = e => {
-		e.preventDefault();
-		const location = e.target.location.value;
-		const query = queryString.stringify({ location: location });
-		this.props.history.push(`/restaurants?${ query }`);
-	}
+	//handleSubmit = e => {
+		//e.preventDefault();
+		//const location = e.target.location.value;
+		//const query = queryString.stringify({ location: location });
+		//this.props.history.push(`/restaurants?${ query }`);
+	//}
 
 	render() {
 		return (
-			<form onSubmit={ this.handleSubmit }>
-				<label htmlFor="location">
-					Location
-					<input id="location" type="text" name="location" />
-				</label>
-				<label htmlFor="food">
-					Food Mood
-					<input id="food" type="text" name="food" />
-				</label>
-				<button type="submit">Search</button>
-			</form>
+			<React.Fragment>
+				<form /*onSubmit={  }*/>
+					<label htmlFor="location">
+						Location
+						<Autocomplete />
+					</label>
+					<label htmlFor="food">
+						Food Mood
+						<Autocomplete />
+					</label>
+					<button type="submit">Search</button>
+				</form>
+			</React.Fragment>
 		)
 	}
 }
