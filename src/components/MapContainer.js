3c19a4c9b623325682bddf631dfc4e8dfb5c9ae3
nedakinduryte <neda.kinduryte@gmail.com>
2019-07-17 19:45:00 +0100

    Container position "relative" to fix overflow

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
-
 import PropTypes from "prop-types";
 
 class MapContainer extends React.Component {
@@ -16,12 +15,92 @@ class MapContainer extends React.Component {
         {restaurants.length > 0 && (
           <Map
             google={this.props.google}
-            zoom={12}
-            style={{ position: "absolute", width: "75%", height: "calc(100% - 64px)" }}
+			zoom={12}
+			containerStyle={{ position: "relative" }}
             initialCenter={{
               lat: restaurants[0].location.latitude,
               lng: restaurants[0].location.longitude
-            }}
+			}}
+			styles={[
+				{
+					"featureType": "administrative",
+					"elementType": "labels.text.fill",
+					"stylers": [
+						{
+							"color": "#444444"
+						}
+					]
+				},
+				{
+					"featureType": "landscape",
+					"elementType": "all",
+					"stylers": [
+						{
+							"color": "#f2f2f2"
+						}
+					]
+				},
+				{
+					"featureType": "poi",
+					"elementType": "all",
+					"stylers": [
+						{
+							"visibility": "off"
+						}
+					]
+				},
+				{
+					"featureType": "road",
+					"elementType": "all",
+					"stylers": [
+						{
+							"saturation": -100
+						},
+						{
+							"lightness": 45
+						}
+					]
+				},
+				{
+					"featureType": "road.highway",
+					"elementType": "all",
+					"stylers": [
+						{
+							"visibility": "simplified"
+						}
+					]
+				},
+				{
+					"featureType": "road.arterial",
+					"elementType": "labels.icon",
+					"stylers": [
+						{
+							"visibility": "off"
+						}
+					]
+				},
+				{
+					"featureType": "transit",
+					"elementType": "all",
+					"stylers": [
+						{
+							"visibility": "off"
+						}
+					]
+				},
+				{
+					"featureType": "water",
+					"elementType": "all",
+					"stylers": [
+						{
+							"color": "#ffffff"
+						},
+						{
+							"visibility": "on"
+						}
+					]
+				}
+			]}
           >
             {restaurants.map(restaurant => {
               return (
