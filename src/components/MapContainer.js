a76d1b4889a268e60150d0f28bc2314539e73044
nedakinduryte <neda.kinduryte@gmail.com>
2019-07-18 22:24:41 +0100

    Changed map style

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
 import PropTypes from "prop-types";
+import mapStyles from "../MapStyles.js";
 
 class MapContainer extends React.Component {
   onClick = name => {
@@ -14,6 +15,7 @@ class MapContainer extends React.Component {
       <React.Fragment>
         {restaurants.length > 0 && (
           <Map
+			styles={mapStyles}
             google={this.props.google}
 			zoom={12}
 			containerStyle={{ position: "relative" }}
