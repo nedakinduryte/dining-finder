71dd90ddecbaf00798a78ab95fa9a8a1403d7013
nedakinduryte <neda.kinduryte@gmail.com>
2019-07-20 13:53:20 +0100

    Added an arrow button & connected to arrowOnClick

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -15,8 +15,17 @@ const theme = createMuiTheme({
 });
 
 const styles = {
-    info:{
-       padding: "0 16px"
+    grid: {
+        display: "grid",
+        gridTemplateColumns: "350px 50px",
+        gridTemplateRows: "1fr",
+        gridTemplateAreas: `
+            "restaurant next"
+        `
+    },
+    info: {
+        padding: "0 16px",
+        gridArea: "restaurant"
     },
     card: {
         height: "calc(100vh - 64px)",
@@ -40,10 +49,13 @@ const styles = {
         display: "flex",
         flexDirection: "row",
         justifyContent: "flex-start",
-        padding: "6px 16px 10px",
+        padding: "6px 16px 10px"
     },
     button: {
         margin: "0 10px 0 0"
+    },
+    next: {
+        gridArea: "next"
     }
 };
 
@@ -61,60 +73,84 @@ class Restaurant extends React.Component {
                             image={restaurant.featured_image}
                             title={restaurant.name}
                         />
-                        <CardContent className={classes.info}>
-                            <h1 className={classes.header}>
-                                {restaurant.name}
-                            </h1>
-                            <p className={classes.subheader}>
-                                {restaurant.cuisines}
-                            </p>
-                            <p>
-                                <span className={classes.label}>User rating:{" "}</span>
-                                <span
-                                    style={{
-                                        color: `#${
-                                            restaurant.user_rating.rating_color
-                                        }`
-                                    }}
-                                >
-                                    {restaurant.user_rating.rating_text} (
-                                    {restaurant.user_rating.aggregate_rating} /
-                                    5)
-                                </span>
-                            </p>
-                            <p>
-                                <span className={classes.label}>Average cost for two:{" "}</span>
-                                {restaurant.average_cost_for_two}
-                                {restaurant.currency}
-                            </p>
-                            <p>
-                                <span className={classes.label}>Address:</span>
-                                <div>
-                                    {restaurant.location.locality}
-                                    <br />
-                                    {restaurant.location.zipcode}
-                                    <br />
-                                    {restaurant.location.city}
+                        <div className={classes.grid}>
+                            <div className={classes.info}>
+                                <CardContent>
+                                    <h1 className={classes.header}>
+                                        {restaurant.name}
+                                    </h1>
+                                    <p className={classes.subheader}>
+                                        {restaurant.cuisines}
+                                    </p>
+                                    <p>
+                                        <span className={classes.label}>
+                                            User rating:{" "}
+                                        </span>
+                                        <span
+                                            style={{
+                                                color: `#${
+                                                    restaurant.user_rating
+                                                        .rating_color
+                                                }`
+                                            }}
+                                        >
+                                            {restaurant.user_rating.rating_text}{" "}
+                                            (
+                                            {
+                                                restaurant.user_rating
+                                                    .aggregate_rating
+                                            }{" "}
+                                            / 5)
+                                        </span>
+                                    </p>
+                                    <p>
+                                        <span className={classes.label}>
+                                            Average cost for two:{" "}
+                                        </span>
+                                        {restaurant.average_cost_for_two}
+                                        {restaurant.currency}
+                                    </p>
+                                    <p>
+                                        <span className={classes.label}>
+                                            Address:
+                                        </span>
+                                        <div>
+                                            {restaurant.location.locality}
+                                            <br />
+                                            {restaurant.location.zipcode}
+                                            <br />
+                                            {restaurant.location.city}
+                                        </div>
+                                    </p>
+                                </CardContent>
+                                <div className={classes.buttons}>
+                                    <Button
+                                        variant="contained"
+                                        className={classes.button}
+                                        href={restaurant.menu_url}
+                                        color="primary"
+                                    >
+                                        See menu
+                                    </Button>
+                                    <Button
+                                        variant="contained"
+                                        disabled={restaurant.has_table_booking}
+                                        href={restaurant.url}
+                                        color="primary"
+                                    >
+                                        Book a table
+                                    </Button>
                                 </div>
-                            </p>
-                        </CardContent>
-                        <div className={classes.buttons}>
-                            <Button
-                                variant="contained"
-                                className={classes.button}
-                                href={restaurant.menu_url}
-                                color="primary"
-                            >
-                                See menu
-                            </Button>
-                            <Button
-                                variant="contained"
-                                disabled={restaurant.has_table_booking}
-                                href={restaurant.url}
-                                color="primary"
-                            >
-                                Book a table
-                            </Button>
+                            </div>
+                            <div className={classes.next}>
+                                <Button
+                                    variant="outlined"
+                                    className={classes.button}
+                                    onClick={this.props.arrowOnClick}
+                                >
+                                    >
+                                </Button>
+                            </div>
                         </div>
                     </Card>
                 </ThemeProvider>
