d336ad416eabfb8fff17834c4752dac5f64a547e
nedakinduryte <neda.kinduryte@gmail.com>
2019-07-25 21:15:11 +0100

    Made code pretty.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Search from './components/Search';
-import Result from './components/Result';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Search from "./components/Search";
+import Result from "./components/Result";
 
 const App = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route path="/" component={Search} exact />
-      <Route path="/restaurants" component={Result} />
-    </Switch>
-  </BrowserRouter>
-)
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" component={Search} exact />
+            <Route path="/restaurants" component={Result} />
+        </Switch>
+    </BrowserRouter>
+);
 
 export default App;
